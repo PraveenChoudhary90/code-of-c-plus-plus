02dbebc8d1726b11791cd57dbddd0d387e97ec21
praveen choudhary <pc852180@gmail.com>
2024-09-26 16:00:50 +0530

    simple all Question do while loop

---
+++ loops/simple_all.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main()
+{  // congequtive number
+    // int number = 1;
+    // do{
+    //     cout << number << "\t";
+    // number++;
+    // }
+    // while(number <= 10);
+
+
+
+
+
+    //squear root
+    // int number = 1;
+    // do{
+    //     cout << number*number << "\t";
+    // number++;
+    // }
+    // while(number <= 10);
+
+
+
+
+
+    //reverse number
+    // int number = 10;
+    // do{
+    //     cout << number << "\t";
+    // number--;
+    // }
+    // while(number >= 1);
+
+
+    // // evennumber
+    // int number = 1;
+    // do{
+    //     if(number % 2 == 0){
+    //     cout << number << "\t";
+    //     }
+    // number++;
+    // }
+    // while(number <= 10);
+
+
+
+
+    // odd number
+    int number = 1;
+    do{
+        if(number % 2 != 0){
+        cout << number << "\t";
+        }
+    number++;
+    }
+    while(number <= 10);
+}
