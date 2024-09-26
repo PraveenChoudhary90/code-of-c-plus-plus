4181158f393b0f34a5a3f0592b86f05b8cbcd523
praveen choudhary <pc852180@gmail.com>
2024-09-26 13:28:17 +0530

    do_while_loop

---
+++ loops/congequtive_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int d =1;
+    do{
+        cout << d << "\t";
+        d++;
+    }
+    while(d <= 10);
+}
---
+++ loops/even_number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int d = 0;
+    do{
+        if(d % 2 ==0){
+            cout << d << "\t"; 
+        }
+        d++;
+
+
+    }
+    while(d <=10);
+}
