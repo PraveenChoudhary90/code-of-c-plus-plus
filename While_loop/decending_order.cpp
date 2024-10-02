e6c5ddd12586de656a9fcfe605fc25f02c799f5f
praveen choudhary <pc852180@gmail.com>
2024-10-02 13:30:17 +0530

    while loop statment question

---
+++ While_loop/decending_order.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 20;
+    while(number >=1){
+        cout << number << "\n";
+        number--;
+
+    }
+    return 0;
+}
---
+++ While_loop/divisble.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 1;
+    while(number <= 20){
+        if(number % 5 == 0){
+            cout << number << "\n";
+        }
+        number++;
+    
+
+}
+}
