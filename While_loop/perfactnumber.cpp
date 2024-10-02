1e158bb65d5cf11669c1447700f49e30627cf904
praveen choudhary <pc852180@gmail.com>
2024-10-02 16:18:35 +0530

    while_loop

--- While_loop/perfactnumber.cpp
+++ While_loop/perfactnumber.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int main(){
     int number = 0;
+    int i = 1;
+    int sum = 0;
+    cout << "Enter the number" << "\n";
+    cin >> number;
+    while(i < number){
+        if(number % i ==0)
+            sum = sum + i;
+            i++;
+    }
+    if(sum == number){
+        cout << i << "Its a perfact number" << "\n";
+    }
+    else{
+        cout << i << "This not a perfact number" << "\n";
+    }
+             
     
+    return 0;
 }
