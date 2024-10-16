f2832ab7b2305e61e01435b8728605c52a5f8e4b
praveen choudhary <pc852180@gmail.com>
2024-10-16 14:28:33 +0530

    viva question

---
+++ test/question2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 90;
+    int b = 1;
+    switch(b--){
+        case1:
+        {
+            cout << "welcome" << "\n";
+            break;
+        }
+        case2:
+        {
+            cout << "bhicome" << "\n";
+            break;
+        }
+        default:
+        {
+            cout << "praveen" << "\n";
+            break;
+        }
+
+    }
+
+}
