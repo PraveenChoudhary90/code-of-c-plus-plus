66fe1c82855848b45b4a04d7e854b4e0c8168374
praveen choudhary <pc852180@gmail.com>
2024-10-21 13:34:17 +0530

    patterns

---
+++ patterns/floyd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t=1;
+    for(int r=1;r<=3;r++)
+    {
+        for(int c=1;c<=r;c++)
+        {
+            cout << t << "\t";
+            t++;
+        }
+        cout << "\n";
+
+    }
+}
