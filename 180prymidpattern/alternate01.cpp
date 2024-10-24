1039b4655c59e28647300213b575d76011722ee8
praveen choudhary <pc852180@gmail.com>
2024-10-24 15:46:04 +0530

    alternate number

--- 180prymidpattern/alternate01.cpp
+++ 180prymidpattern/alternate01.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 int main()
 {
-
-    for(int r = 1;r<=3;r++)
-    {
-        for(int s =3;s>r;s--)
-        {
+   for (int i = 1; i <= 5; i++) {
+        for(int s = 5;s>i;s--){
             cout << " ";
+
         }
-     for(int c=r;c>=1;c--)
-        {
-            cout << c ;
+
+        for (int j = i; j >=1; j--) {
+            
+            if (j % 2 == 0) {
+                cout << "0 ";
+            } else {
+                cout << "1 ";
+            }
         }
         cout << "\n";
     }
+
     return 0;
 }
