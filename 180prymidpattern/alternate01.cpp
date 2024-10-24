742968cf5cb29322bc43161afa9547d22b3ca5bd
praveen choudhary <pc852180@gmail.com>
2024-10-24 13:39:19 +0530

    prymid pattern

---
+++ 180prymidpattern/alternate01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for(int r = 1;r<=3;r++)
+    {
+        for(int s =3;s>r;s--)
+        {
+            cout << " ";
+        }
+     for(int c=r;c>=1;c--)
+        {
+            cout << c ;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
