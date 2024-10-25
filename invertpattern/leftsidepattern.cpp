17e904bc5ae5b3bccc1b0039ae1224b7afb56064
praveen choudhary <pc852180@gmail.com>
2024-10-25 14:51:34 +0530

    invert pattern

---
+++ invertpattern/leftsidepattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int r = 1;r<=5;r++)
+    {
+        for(int s = 1;s<r;s++)
+        {
+            cout << " ";
+
+        }
+        for(int c= 5;c>=r;c--)
+        {
+            cout << "*" ;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
