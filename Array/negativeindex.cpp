2461042f6a9bad0408895e57ca8de3e1da5d009f
praveen choudhary <pc852180@gmail.com>
2024-11-09 13:44:09 +0530

    negative index in array

---
+++ Array/negativeindex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {12,13,11,43,5,66,77};
+    int s = sizeof(arr)/sizeof(arr[0]);
+    cout << -4[arr] << "\n";
+    arr[4] = -4[arr];
+    for(int i =0;i<s;i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
