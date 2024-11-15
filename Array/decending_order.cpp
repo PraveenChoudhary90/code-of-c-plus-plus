3485afa1299650f1d2a41aebf756f04b284c5e22
praveen choudhary <pc852180@gmail.com>
2024-11-15 23:30:24 +0530

    decending order

--- Array/decending_order.cpp
+++ Array/decending_order.cpp
@@ -5,7 +5,28 @@ int main()
  int arr[] = {7,4,6,11,5};
  int len = sizeof(arr)/sizeof(arr[0]);
  for(int i = 0; i<len;i++)
+
  {
+
+    cout << arr[i] << "\t";
     
+ }
+ cout << "after\n";
+ for(int i = 0;i<len;i++)
+ {
+    for(int j = 0;j<len-1;j++)
+    {
+        int temp;
+        if(arr[j]<arr[j+1])
+        {
+            temp = arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+        }
+    }
+ }
+ for(int  i = 0;i<len;i++)
+ {
+    cout << arr[i] << "\t";
  }
 }
