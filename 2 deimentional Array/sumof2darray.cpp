b1bafb8d0ae3d6dc02063fa2dd3673f3289103b8
praveen choudhary <pc852180@gmail.com>
2024-11-18 15:57:11 +0530

    Sum of array in 2d array

---
+++ 2 deimentional Array/sumof2darray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[2][3];
+    int sum = 0;
+    cout << "Enter the number" << "\n";
+    for(int r = 0; r <2;r++)
+    {
+        for(int c =0;c<3;c++)
+        {
+            cin >> arr[r][c];
+        }
+    }
+    for(int r = 0; r <2;r++)
+    {
+        for(int c =0;c<3;c++)
+        {
+            int a = arr[r][c];
+            cout <<  arr[r][c] << "\t";
+             sum = sum + a;
+        }
+        cout << "sum=" << sum << "\n";
+       
+    }
+    
+    return 0;
+
+}
