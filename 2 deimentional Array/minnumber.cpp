93e521c2f9ac3770c78ce0f2fdc7afded89e10ac
praveen choudhary <pc852180@gmail.com>
2024-11-18 16:05:55 +0530

    minimunumber in 2d array

---
+++ 2 deimentional Array/minnumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[2][3];
+    int min = INT_MAX;
+    for(int i = 0;i<2;i++)
+    {
+        for(int j= 0;j<3;j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+     for(int i = 0;i<2;i++)
+    {
+        for(int j= 0;j<3;j++)
+        {
+            if(min > arr[i][j])
+            {
+                min =   arr[i][j];
+                break;
+            }
+            cout <<  min << "\t";
+        }
+        cout << "\n";
+    }
+}
