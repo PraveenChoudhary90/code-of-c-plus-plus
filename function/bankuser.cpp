0134aba35c92e2b4b4973e2fbf76e99239fe9ee5
praveen choudhary <pc852180@gmail.com>
2024-12-09 15:30:02 +0530

    function pointer using function

--- function/bankuser.cpp
+++ function/bankuser.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
-int axis (int , int p){
+int axis (int cardnumber , int password){
+    return 10000;
                                                               
+}
+int sbi(int cardnumber , int password){
+    return 20000;
+}
+int main(){
+
+    int (*atm)(int ,int );
+      atm = sbi;
+      cout << "balence=" << atm(3,4);
+      atm = axis;
+      cout << "balence=" << atm(3,5);
+
 }
