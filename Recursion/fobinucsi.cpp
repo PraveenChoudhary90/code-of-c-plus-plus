30f9faf1021cfb964f5751bc79d2c71a3d648905
praveen choudhary <pc852180@gmail.com>
2024-12-13 13:23:11 +0530

    fibonacci serese

--- Recursion/fobinucsi.cpp
+++ Recursion/fobinucsi.cpp
@@ -4,12 +4,15 @@ int fact(int a){
     if(a==0){
         return 1;
     }
-    int r = fact(a-1)*fact(a-2);
+    if(a<0){
+        return 0;
+    }
+    int r = fact(a-1)+fact(a-2);
     return r;
 }
 int main(){
     int a;
     cout << "Enter the number " << "\n";
     cin >> a;
-    cout << fact(a) << "\n";
+    cout << "fibonacci nth term serise =" <<  fact(a) << "\n";
 }
