683eb26957e61e229c03f38911389f4cce34758a
praveen choudhary <pc852180@gmail.com>
2024-12-17 22:28:53 +0530

    added single class type

---
+++ opps Class And Object/singleclass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class top{
+    int a =110;
+    int b =190;
+  public:void sum(){
+    cout << a+b << "\n";
+  }
+};
+int main()
+{
+    top k;
+    k.sum();
+}
