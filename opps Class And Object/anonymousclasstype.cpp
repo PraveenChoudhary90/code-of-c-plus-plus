03d6b139a09aaf9f820d1f2f580dce5e6bcc4c33
praveen choudhary <pc852180@gmail.com>
2024-12-17 13:29:43 +0530

    Function and class types

---
+++ opps Class And Object/anonymousclasstype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class 
+{
+    int a=10;
+    int b=90;
+    public:void show(){
+        cout << a+b << "\n";
+    }
+}t;
+int main(){
+    t.show();
+}
---
+++ opps Class And Object/staticfunction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class top{
+    static int a;
+    static int b;
+    public:static void sum()
+    {
+        cout << a+b;
+
+    }
+};
+ int top::a=100;
+ int top::b=100;
+int main(){
+    top::sum();
+   
+}
