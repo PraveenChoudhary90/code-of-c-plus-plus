068dd157bdcb368b9a33b57fd500417c10308f4e
praveen choudhary <pc852180@gmail.com>
2024-12-23 13:46:26 +0530

    Shallow and deep copy

---
+++ opps Class And Object/Shallowpointercopy.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include <string.h>
+class student{
+    char *c;
+    public:student (char *s)
+    {
+        c=new char[20];
+        strcpy(c,s);
+    }
+    // student(student &obj){
+    //     c=new char[20];
+    //     strcpy(c,obj.c);
+    // }
+    void show(){
+        cout << "name=" << c << "\n";
+    }
+    void surname(char *s){
+        strcat(c,s);
+    }
+};
+int main(){
+    student s1("praveen");
+    s1.show();
+    student s2(s1);
+    s2.show();
+    s2.surname("choudhary");
+    cout << "updated by s1 \n";
+    s1.show();
+    cout << "upadetd by s2 \n";
+    s2.show();
+}
