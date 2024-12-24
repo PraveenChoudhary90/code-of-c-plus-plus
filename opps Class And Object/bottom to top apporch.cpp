ad1f2508affcc8f32187ad617dd63466267faaa3
praveen choudhary <pc852180@gmail.com>
2024-12-24 13:21:51 +0530

    bottom to tp apporch

---
+++ opps Class And Object/bottom to top apporch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class hope{
+    public:hope()
+    {
+        cout << "good" << "\n";
+
+    }
+} t;
+int main()
+{
+    cout << "hello" << "\n";
+}
---
+++ opps Class And Object/bottomtotop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello" << "\n";
+}
+class hope{
+    public:hope()
+    {
+        cout << "good" << "\n";
+
+    }
+} t;
