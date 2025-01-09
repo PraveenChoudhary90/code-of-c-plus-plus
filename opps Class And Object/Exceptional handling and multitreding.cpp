11d677340ad3d2df9dd77e7c33a8b5464a6ea64a
praveen choudhary <pc852180@gmail.com>
2025-01-09 13:35:11 +0530

    Exception handling

---
+++ opps Class And Object/Exceptional handling and multitreding.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//  int  a=9;
+//   int  b=0;//exception handing is coming
+//     cout<<a/b;
+//     cout << "done" << "\n";
+// }
+// try block and catch block
+int main(){
+    int a,b;
+    cout << "Enter the frist number" << "\n";
+    cin >> a;
+    cout << "Enter the second number" << "\n";
+    cin >> b;
+    try{
+        if(b<=0){
+            throw b;
+        }
+        cout << a/b  << "\n";
+    }
+    catch(int b){
+        cout << "denominator can not be zero";
+    }
+    cout << "done" << "\n";
+}
